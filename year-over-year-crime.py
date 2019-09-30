e595ed1c7eb0cdb592b60b43df2c124ea02d5c0c
Rachel Poulos <Rachel.Poulos@ey.com>
2019-09-30 06:22:21 -0400

    make function

--- year-over-year-crime.py
+++ year-over-year-crime.py
@@ -6,9 +6,10 @@ import requests
 from pprint import pprint
 import matplotlib.lines as lines
 
-response = requests.get('https://arcgis.atlantaregional.com/arcgis/rest/services/OpenData/FeatureServer/196/query?where=1%3D1&outFields=*&outSR=4326&f=json')
+response = requests.get('https://gis.atlantaga.gov/dpcd/rest/services/OpenDataService/FeatureServer/3/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryPolyline&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&gdbVersion=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=&f=geojson')
 data = response.json()
 data = data["features"]
+
 crime_file = os.path.join("Resources", "COBRA-2009-2018.csv")
 crime_df = pd.read_csv(crime_file)
 
@@ -20,13 +21,17 @@ crime_2014_df = crime_df.loc[crime_df["Year"] == "2014"]
 # crime_2017_df = crime_df.loc[crime_df["Year"] == "2017"]
 # crime_2018_df = crime_df.loc[crime_df["Year"] == "2018"]
 
-x = crime_2014_df["Latitude"]
-y = crime_2014_df["Longitude"]
+x = crime_2014_df["Longitude"]
+y = crime_2014_df["Latitude"]
 
-plt.scatter(y, x, alpha=.01)
+plt.scatter(x, y, alpha=.005)
+
+npu_outlines = []
 for npu in data:
-	rings = npu["geometry"]["rings"]
-	x = [ring[0] for ring in rings[0]]
-	y = [ring[1] for ring in rings[0]]
+	npu_outlines.append(npu["geometry"]["coordinates"][0])
+
+for value in npu_outlines:
+	x = [outline[0] for outline in value]
+	y = [outline[1] for outline in value]
 	plt.plot(x, y)
 plt.show()
